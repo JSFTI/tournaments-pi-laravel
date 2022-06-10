6e9a225add59ae6e360323248efcb4c60a827571
JSFTI <jasonsuryaf@gmail.com>
2022-06-10 23:08:52 +0700

    Fix allowed origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3333', 'https://tournaments-pi.herokuapp.com/'],
+    'allowed_origins' => ['http://localhost:3333', 'https://tournaments-pi.herokuapp.com'],
 
     'allowed_origins_patterns' => [],
 
